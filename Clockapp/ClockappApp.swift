b1d43924db8d67ce40a0cf39796da97d2ad1f8f2
Alex Hutchinson <Ahutchinson@lcs.on.ca>
2026-02-24 14:08:15 -0500

    Initial Commit

---
+++ Clockapp/ClockappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockappApp.swift
+//  Clockapp
+//
+//  Created by Alexander Hutchinson on 2026-02-24.
+//
+
+import SwiftUI
+
+@main
+struct ClockappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
