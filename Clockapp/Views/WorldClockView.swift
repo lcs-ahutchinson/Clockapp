9dae6ac019e8f02304e77a73e398806cd161ac3c
Alex Hutchinson <Ahutchinson@lcs.on.ca>
2026-02-24 15:08:55 -0500

    added different clocks to world view page

--- Clockapp/Views/WorldClockView.swift
+++ Clockapp/Views/WorldClockView.swift
@@ -10,7 +10,61 @@ import SwiftUI
 struct WorldClockView: View {
     var body: some View {
         NavigationStack    {
-            Text("World Clock")
+            VStack{
+                //Text("World Clock")
+                
+                
+                    VStack{
+                        HStack{
+                            Spacer().frame(width:3)
+                            VStack(alignment:.leading){
+                                Text("Today, +0HRS")
+                                Text("Ottawa")
+                                    .font(.system(size: 36))
+                            }
+                            Spacer()
+                            Text("6:35AM")
+                                .font(.system(size: 50))
+                        }
+                        Spacer().frame(height:5)
+                        HStack{
+                            Spacer().frame(width:3)
+                            VStack(alignment:.leading){
+                                Text("Today, -3HRS")
+                                Text("Vancouver")
+                                    .font(.system(size: 36))
+                            }
+                            Spacer()
+                            Text("3:35AM")
+                                .font(.system(size: 50))
+                        }
+                        Spacer().frame(height:5)
+                        HStack{
+                            Spacer().frame(width:3)
+                            VStack(alignment:.leading){
+                                Text("Today, +6HRS")
+                                Text("Hamburg")
+                                    .font(.system(size: 36))
+                            }
+                            Spacer()
+                            Text("12:35AM")
+                                .font(.system(size: 50))
+                        }
+                        Spacer().frame(height:5)
+                        HStack{
+                            Spacer().frame(width:3)
+                            VStack(alignment:.leading){
+                                Text("Today, +16HRS")
+                                Text("Sydney")
+                                    .font(.system(size: 36))
+                            }
+                            Spacer()
+                            Text("10:35AM")
+                                .font(.system(size: 50))
+                        }
+                    }
+                
+            }
                 .navigationTitle("World Clock")
                 .toolbar{
                     ToolbarItem(placement: .topBarLeading){
